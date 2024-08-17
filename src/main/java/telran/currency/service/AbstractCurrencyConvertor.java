2cbee3adb32e9bf2659814ffafb19e7b17488c00
AlekseiKorotkov159 <158587440+AlekseiKorotkov159@users.noreply.github.com>
2024-08-17 19:47:07 +0300

    CW #46 Project structure

---
+++ src/main/java/telran/currency/service/AbstractCurrencyConvertor.java
@@ -0,0 +1,26 @@
+package telran.currency.service;
+
+import java.util.*;
+
+public class AbstractCurrencyConvertor implements CurrencyConvertor {
+protected Map<String, Double> rates; //key - currency ISO code;
+//value - amount of code's units in 1 EUR
+	@Override
+	public List<String> strongestCurrencies(int amount) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<String> weakestCurrencies(int amount) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double convert(String codeFrom, String codeTo, int amount) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
